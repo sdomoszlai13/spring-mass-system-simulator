e1e7ab11711cdfb1800a49b913077d4bea218327
sdomoszlai13 <sebastian.domoszlai@protonmail.com>
2023-01-30 12:06:56 +0100

    Added update and some other functions.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Fixture:
     def __init__(self, i_pos):
         self.pos[0] = i_pos[0]
@@ -7,8 +10,8 @@ class Fixture:
 class Mass:
     def __init__(self, i_m, i_pos, i_v):
         self.m = i_m
-        self.pos[0] = [i_pos[0], i_pos[1]]
-        self.v[0] = [i_v[0], i_v[1]]
+        self.pos = [i_pos[0], i_pos[1]]
+        self.v = [i_v[0], i_v[1]]
 
 
 class Spring:
@@ -26,21 +29,44 @@ class Spring:
     
     def connectMassesAndFixtures(self):
         pass
+
     def connectMassesAndSprings(self):
         pass
 
 
 class SpringMassSystem:
-    def __init__(self, masses, springs, timestep, gravity):
+    def __init__(self, masses, springs, timesteps, gravity):
         self.masses = masses
         self.springs = springs
-        self.timestep = timestep
+        self.timesteps = timesteps
         self.gravity = gravity
+
     def run(self):
-        pass
+        self.times = np.linspace(0, 1, self.timesteps)
+        for t in times:
+            for m in masses:
+                updateXY(m)
+                updateTrajectory(m)
+
+    def updateF(self):
+        """Update force acting on a mass."""
+        for m in self.masses:
+            f[0] = -k * self.mass.pos[0]
+            f[1] = 
+    
+    def update(self):
+        """"Update position and velocity of a mass."""
+
+        delta_t = 1 / self.timesteps
+        self.m.pos[0] += self.m.v[0] * delta_t
+        self.m.pos[1] += self.m.v[1] * delta_t
+
+
+        self.m.v[0] += f / self.m.m * delta_t
+        self.m.v[1] += f / self.m.m * delta_t
 
 
 class Trajectory:
     def __init__(self, mass):
-        self.x = mass.pos[0][0]
-        self.y = mass.pos[0][1]
+        self.x = mass.pos[0]
+        self.y = mass.pos[1]
