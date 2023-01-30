2b21819563bfa436108f508f2745b1e0d148eeee
sdomoszlai13 <sebastian.domoszlai@protonmail.com>
2023-01-30 10:59:53 +0100

    Added classes Fixture, Mass, Spring, SpringMassSystem and Trajectory.

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-print("Hello, world!")
+class Fixture:
+    def __init__(self, i_pos):
+        self.pos[0] = i_pos[0]
+        self.pos[1] = i_pos[1]
+
+
+class Mass:
+    def __init__(self, i_m, i_pos, i_v):
+        self.m = i_m
+        self.pos[0] = [i_pos[0], i_pos[1]]
+        self.v[0] = [i_v[0], i_v[1]]
+
+
+class Spring:
+    """Creates a spring that connects a fixture and a mass, or two masses.
+    Attributes:
+    -rest length
+    -stiffness parameter
+    -connection points"""
+
+    def __init__(self, l0, k, conn):
+        self.l0 = l0
+        self.k = k
+        self.conn[0] = conn[0]
+        self.conn[1] = conn[1]
+    
+    def connectMassesAndFixtures(self):
+        pass
+    def connectMassesAndSprings(self):
+        pass
+
+
+class SpringMassSystem:
+    def __init__(self, masses, springs, timestep, gravity):
+        self.masses = masses
+        self.springs = springs
+        self.timestep = timestep
+        self.gravity = gravity
+    def run(self):
+        pass
+
+
+class Trajectory:
+    def __init__(self, mass):
+        self.x = mass.pos[0][0]
+        self.y = mass.pos[0][1]
